55f3baba961e9611ac31f7542a9ed52cc115a119
Ketch4n <mangao.christian.04@gmail.com>
2023-10-10 23:20:15 +0800

    October 10

--- lib/api/login.dart
+++ lib/api/login.dart
@@ -1,11 +1,12 @@
-// ignore_for_file: use_build_context_synchronously
 import 'dart:convert';
 import 'package:attendance_monitoring/pages/home.dart';
 import 'package:attendance_monitoring/widgets/show_dialog.dart';
+import 'package:attendance_monitoring/widgets/show_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'server.dart';
 import 'package:http/http.dart' as http;
+
+import 'server.dart';
 
 Future<void> login(
   BuildContext context,
@@ -17,39 +18,56 @@ Future<void> login(
     String message = "Username or Password Empty !";
     await showAlertDialog(context, title, message);
   } else {
-    // HTTP request
-    final response = await http.post(
-      Uri.parse('${Server.host}auth/login.php'),
-      body: {
-        'email': email,
-        'password': password,
-      },
-    );
-    Map<String, dynamic> data = json.decode(response.body);
-    final message = data['message'];
-    final status = "${response.statusCode}";
-    if (response.statusCode == 200) {
-      if (data['success']) {
-        // set id for shared pref
-        final userId = data['id'];
-        final prefs = await SharedPreferences.getInstance();
-        prefs.setString('userId', userId);
-        const title = "Login success";
-        String content = "Welcome $message";
-        await showAlertDialog(context, title, content);
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const Home(),
-          ),
-        );
+    try {
+      // showDialog(
+      //   context: context,
+      //   builder: (BuildContext context) {
+      //     return Center(child: CircularProgressIndicator());
+      //   },
+      //   barrierDismissible: false,
+      // );
+      // HTTP request
+      final response = await http.post(
+        Uri.parse('${Server.host}auth/login.php'),
+        body: {
+          'email': email,
+          'password': password,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = json.decode(response.body);
+        final message = data['message'];
+
+        if (data['success']) {
+          // set id for shared pref
+          final userId = data['id'];
+          final prefs = await SharedPreferences.getInstance();
+          prefs.setString('userId', userId);
+          const title = "Login success";
+          String content = "Welcome $message";
+          await showAlertDialog(context, title, content);
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const Home(),
+            ),
+          );
+        } else {
+          const title = "Login failed";
+          await showAlertDialog(context, title, message);
+        }
       } else {
-        const title = "Login failed";
-        await showAlertDialog(context, title, message);
+        const title = "Failed to log in";
+        await showAlertDialog(
+            context, title, 'HTTP Status Code: ${response.statusCode}');
       }
-    } else {
+    } catch (error) {
       const title = "Failed to log in";
-      await showAlertDialog(context, title, status);
+      await showAlertDialog(context, title, 'Error: $error');
     }
+    // finally {
+    //   Navigator.pop(context); // Close loading indicator in all cases
+    // }
   }
 }
--- lib/api/server.dart
+++ lib/api/server.dart
@@ -1,4 +1,6 @@
 class Server {
   // static String host = "http://localhost/attendance_api/";
-  static String host = "http://192.168.1.5/attendance_api/";
+  // static String host = "http://192.168.1.5/attendance_api/";
+  static String host = "http://10.0.0.34/attendance_api/";
+  // static String host = "http://attendancemonitoring.rf.gd/";
 }
